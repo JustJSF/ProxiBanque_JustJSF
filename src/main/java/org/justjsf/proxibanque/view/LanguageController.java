505c0b846d5b168ed69b0dd3ed99d0ba3b64bdf5
GottaLoveJava <francois.destremau@gmail.com>
2017-09-06 09:20:33 +0200

    i18n + bouton retour clients

---
+++ src/main/java/org/justjsf/proxibanque/view/LanguageController.java
@@ -0,0 +1,30 @@
+package org.justjsf.proxibanque.view;
+
+
+import java.util.Locale;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name="languageController")
+@SessionScoped
+public class LanguageController {
+private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+
+	public String changeLanguage(String language) {
+	locale = new Locale(language);
+		FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+		return "customers/all.xhtml";
+	}
+	public Locale getLocale() {
+		return locale;
+	}
+	public void setLocale(Locale locale) {
+		this.locale = locale;
+	}
+	public String getLanguage() {
+		return locale.getLanguage();
+	}
+
+}
